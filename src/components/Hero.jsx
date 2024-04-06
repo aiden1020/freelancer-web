3679c0ca00cd309cc7951f23d2b439c708a9c78e
aiden1020 <tsztowong@gmail.com>
2024-04-06 19:27:42 +0800

    committed

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero =()=>{
+return (
+    <div class="flex min-h-screen items-center justify-center  ">
+        <div class="w-max">
+            <h1 class="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl text-white font-bold">Coming Soon ...</h1>
+        </div>
+    </div>
+)
+}
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React  ,{ useState } from "react";
+import {AiOutlineClose ,AiOutlineMenu} from 'react-icons/ai'
+
+const Navbar = () =>{
+    const [open , setOpen] =useState([false]);
+    return(
+        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white select-none'>
+        <div className='flex items-center'>
+            <img src="./logo.png" alt="logo" className=" h-[60px] p-1"/>
+            <h1 className='text-3xl font-bold text-white'>Slash</h1>
+        </div>
+
+            <ul className="hidden md:flex font-semibold">
+                <li className="p-4 ">服務</li>
+                <li className="p-4">了解Slash</li>
+                <li className="p-4">下載</li>
+                <li className="p-4">關於我們</li>
+            </ul>
+            <div onClick={() => setOpen(!open)} className="block md:hidden">
+                {open ? <AiOutlineMenu size={25}/>:<AiOutlineClose size={25}/>}
+            </div>
+            <div className={!open ? "fixed left-0 top-24 w-{60%} m-4 ease-in-out duration-500" : "fixed left-[-100%]"}>
+                    <ul className="p-4">
+                        <li className="p-4 border-b border-gray-400">服務</li>
+                        <li className="p-4 border-b border-gray-400">了解Slash</li>
+                        <li className="p-4 border-b border-gray-400">下載</li>
+                        <li className="p-4 border-b border-gray-400">關於我們</li>
+                    </ul>
+            </div>            
+
+        </div>
+
+    )
+}
+export default Navbar;
