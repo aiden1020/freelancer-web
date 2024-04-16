b74c372715c90677ff3f68a59042376c9a13867e
aiden1020 <tsztowong@gmail.com>
2024-04-16 20:30:44 +0800

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className="main ">
         {/* black mask for background video */}
         <div className=" top-0 h-[100%] w-[100%] absolute bg-black opacity-40"/>
-        <video src={Background_Video} autoPlay loop muted className=" h-[100%] w-[100%] object-cover"/>
+        <video src={Background_Video} autoPlay loop muted playsInline className=" h-[100%] w-[100%] object-cover"/>
         <div className=" absolute h-[100%] w-[100%] top-0">
 
           <Navbar />
